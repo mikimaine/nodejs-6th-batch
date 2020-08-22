5815d226537bb031bf343b262979f15f7522cc4d
mikimaine <mikias.amdu@gmail.com>
2020-08-22 18:55:33 +0300

    add modules

---
+++ week_one_nodejs_modules/custom_log.js
@@ -0,0 +1,4 @@
+
+module.exports = (log) => console.log(`this is custom log: ${log}`);
+
+
---
+++ week_one_nodejs_modules/index.js
@@ -0,0 +1,23 @@
+
+
+
+console.log(`type of module ${typeof module}`);   // module.
+
+console.log(`type of exports ${typeof exports}`); // exports.
+
+console.log(`type of require ${typeof require}`); // require()
+
+
+console.log(`Current PLatform ${process.platform}`);
+
+console.log(`Filename ${__filename}`);
+
+console.log(`Dir name ${__dirname}`);
+
+// 
+
+var custom_log = require('./custom_log')
+
+custom_log('test')
+
+
